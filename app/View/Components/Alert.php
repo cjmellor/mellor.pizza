a44bf722784fddfe2ab04d9013366632bc8f9792
Chris Mellor <cmellor@gmail.com>
2021-03-21 19:25:07 +0000

    Added an Alert component

---
+++ app/View/Components/Alert.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class Alert extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @param $type
+     */
+    public function __construct(
+        public $type
+    ) {}
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|string
+     */
+    public function render()
+    {
+        return function (array $data) {
+            $type = $this->setType($this->type);
+
+            return 'components.alert';
+        };
+    }
+
+    /**
+     * Determines the type of alert to use
+     *
+     * @param $type
+     * @return string
+     */
+    public function setType($type): string
+    {
+        return $data['attributes'] = match ($type) {
+            // TODO: change these to Tailwind classes
+            'info' => 'info',
+            'success' => 'success',
+            'warning' => 'warning',
+        };
+    }
+}
