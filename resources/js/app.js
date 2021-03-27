f4fa79a2026db83dcbc3a7181da77678ed639d79
Chris Mellor <cmellor@gmail.com>
2021-03-27 15:39:36 +0000

    Installed a ESLint configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,11 @@
-require('./bootstrap')
+require('./bootstrap');
 
-const endpointBase = process.env.MIX_APP_URL
+const endpointBase = process.env.MIX_APP_URL;
 const endpoints = {
-    delete: endpointBase + '/trix/remove-attachment',
-    post: endpointBase + '/trix/add-attachment',
+    delete: `${endpointBase}/trix/remove-attachment`,
+    post: `${endpointBase}/trix/add-attachment`,
     trix: '/storage/trix-attachments/',
-}
-const csrfToken = document.querySelector('meta[name=\'csrf-token\']').content
+};
+const csrfToken = document.querySelector("meta[name='csrf-token']").content;
 
-export { csrfToken, endpointBase, endpoints }
+export { csrfToken, endpointBase, endpoints };
--- resources/js/trix/add-attachment.js
+++ resources/js/trix/add-attachment.js
@@ -1,27 +1,29 @@
-import { endpointBase, endpoints } from '../app'
+import { endpointBase, endpoints } from '../app';
 
-addEventListener('trix-attachment-add', event => {
-    const attachment = event.attachment
+addEventListener('trix-attachment-add', (event) => {
+    const { attachment } = event;
 
     if (attachment.file) {
-        const data = new FormData()
-        data.append('file', attachment.file)
+        const data = new FormData();
+        data.append('file', attachment.file);
 
-        axios.post(endpoints.post, data, {
-            onUploadProgress: event => {
-                const progress = event.loaded / event.total * 100
-                attachment.setUploadProgress(progress)
-            },
-        }).then(response => {
-            if (response.status === 200) {
-                const url = endpointBase + endpoints.trix + response.data
+        axios
+            .post(endpoints.post, data, {
+                onUploadProgress: (event) => {
+                    const progress = (event.loaded / event.total) * 100;
+                    attachment.setUploadProgress(progress);
+                },
+            })
+            .then((response) => {
+                if (response.status === 200) {
+                    const url = endpointBase + endpoints.trix + response.data;
 
-                const attributes = {
-                    url: url,
-                    href: url + '?content-disposition=attachment',
+                    const attributes = {
+                        url,
+                        href: `${url}?content-disposition=attachment`,
+                    };
+                    attachment.setAttributes(attributes);
                 }
-                attachment.setAttributes(attributes)
-            }
-        })
+            });
     }
-})
+});
