26ee835c12fec274982adf636c02c9f809a92c10
Chris Mellor <cmellor@gmail.com>
2021-03-30 22:00:12 +0100

    Created an AlpineJS component

--- resources/views/components/markdown-editor.blade.php
+++ resources/views/components/markdown-editor.blade.php
@@ -28,7 +28,6 @@
 @endpush
 
 @push('scripts')
-    {{--TODO: Make a 'use-alpine' component--}}
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <x-use-alpine/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.6.0/highlight.min.js" defer></script>
 @endpush
