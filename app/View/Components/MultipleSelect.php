b06892cf501cc01293abe7621868ee756d926526
Chris Mellor <cmellor@gmail.com>
2021-04-03 18:44:00 +0100

    Added a Blade Component for a multi-select field

---
+++ app/View/Components/MultipleSelect.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class MultipleSelect extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @param  string  $id
+     * @param  string|null  $name
+     */
+    public function __construct(
+        public string $id,
+        public ?string $name = null,
+    )
+    {}
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.multiple-select');
+    }
+}
---
+++ resources/views/components/multiple-select.blade.php
@@ -0,0 +1,36 @@
+{{--Template--}}
+<select {{ $attributes }}
+        id="{{ $id }}"
+        multiple
+        name="{{ $name ?? \Str::singular($id) }}_id[]"
+        x-data
+        x-init="multiSelect($refs.select)"
+        x-ref="select">
+    {{ $slot }}
+</select>
+{{--End Template--}}
+
+{{--Scripts--}}
+@push('scripts')
+    <script defer src="https://cdn.jsdelivr.net/npm/tom-select@1.1/dist/js/tom-select.complete.min.js"></script>
+
+    <script>
+        const multiSelect = ref => {
+            const options = {
+                plugins: [
+                    'no_backspace_delete',
+                    'remove_button',
+                ]
+            }
+
+            new TomSelect(ref, options)
+        }
+    </script>
+@endpush
+{{--End Scripts--}}
+
+{{--Styles--}}
+@push('stylesheets')
+
+@endpush
+{{--End Styles--}}
