df546b021a11ac7d17a44a5e9a7ce6e6e2015b8a
Chris Mellor <cmellor@gmail.com>
2021-04-03 18:46:28 +0100

    Installed a Multi Select package - Tom Select
    - bundled autoprefixer PostCSS plugin

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix')
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,11 @@ const mix = require('laravel-mix')
  |
  */
 
-mix.js(['resources/js/app.js', 'resources/js/trix/add-attachment.js', 'resources/js/trix/remove-attachment.js'], 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ])
+mix.js(
+    [
+        'resources/js/app.js',
+        'resources/js/trix/add-attachment.js',
+        'resources/js/trix/remove-attachment.js',
+    ],
+    'public/js'
+).postCss('resources/css/app.css', 'public/css', [require('autoprefixer')]);
