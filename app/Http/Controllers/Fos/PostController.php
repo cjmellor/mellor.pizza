a8fb6d297fbedf755ce970f4f5b8b03a60f18b4c
Chris Mellor <cmellor@gmail.com>
2021-04-07 22:40:05 +0100

    Add store and update logic to Post controller

--- app/Http/Controllers/Fos/PostController.php
+++ app/Http/Controllers/Fos/PostController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers\Fos;
 
 use App\Http\Controllers\Controller;
-use App\Http\ViewComposers\Fos\PostComposer;
+use App\Http\Requests\Fos\PostRequest;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\Tag;
-use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class PostController extends Controller
@@ -28,7 +27,8 @@ class PostController extends Controller
     {
         $posts = Post::with('author')->latest()->get();
 
-        return view('fos.posts.index', ['posts' => $posts]);
+        return view('fos.posts.index')
+            ->with('posts', $posts);
     }
 
     /**
@@ -44,12 +44,32 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  \App\Http\Requests\Fos\PostRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Throwable
      */
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
-        //
+        $post = new Post();
+
+        $post->author()->associate(auth()->id());
+        $post->category()->associate($request->category_id);
+
+        $post->fill($request->validated());
+        $post->slug = Str::slug($request->title);
+
+        // If the post is Markdown, update model
+        if ($post->content_type === 'markdown') {
+            $post->is_markdown = true;
+        }
+
+        $post->saveOrFail();
+
+        // Add tags if they don't exist, otherwise, update the model
+        $post->tags()->sync($this->tagsOrCreate($request));
+
+        return redirect()->route('posts.index')
+            ->with('alert_status', 'New post created');
     }
 
     /**
@@ -60,7 +80,8 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        return view('fos.posts.show', ['post' => $post]);
+        return view('fos.posts.show')
+            ->with('post', $post);
     }
 
     /**
@@ -78,31 +99,56 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  Request  $request
+     * @param  \App\Http\Requests\Fos\PostRequest  $request
      * @param  Post  $post
      * @return \Illuminate\Http\RedirectResponse
      * @throws \Throwable
      */
-    public function update(Request $request, Post $post)
+    public function update(PostRequest $request, Post $post)
     {
         if ($request->is_published) {
-            $post->fill(['is_published' => true]);
+            $post->is_published = true;
         }
 
-        $post->title = $request->title;
+        $post->fill($request->validated());
         $post->slug = Str::slug($request->title);
-        $post->excerpt = $request->excerpt;
-        $post->body = $request->post_content;
-        // $post->post_image' = $request->post_image;
 
         $post->category()->associate($request->category_id);
 
         $post->saveOrFail();
 
-        $post->tags()->sync($request->tag_id);
+        $post->tags()->sync($this->tagsOrCreate($request));
 
         return redirect()->route('posts.show', $post)
             ->with('alert_status', 'Blog post has been updated');
+    }
+
+    /**
+     * Get a list of tag IDs from the request.
+     * If a tag doesn't exist, it is created
+     * -----
+     * Code inspired by @themsaid
+     * https://github.com/themsaid/wink/blob/1.x/src/Http/Controllers/PostsController.php#L115-L129
+     *
+     * @param $request
+     * @return array
+     */
+    public function tagsOrCreate($request): array
+    {
+        $tags = Tag::all();
+
+        return collect($request->tag_id)
+            ->map(function ($postTags) use ($tags): string {
+                $tag = $tags->firstWhere('id', $postTags);
+
+                if (is_null($tag)) {
+                    $tag = Tag::create([
+                        'name' => Str::slug($postTags),
+                    ]);
+                }
+
+                return (string) $tag->id;
+            })->toArray();
     }
 
     /**
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -10,6 +10,8 @@ class Tag extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name'];
+
     /**
      * A 'Tag' belongs to many 'Posts'
      *
