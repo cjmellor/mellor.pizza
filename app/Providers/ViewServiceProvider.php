013a9ff7e1140ba4a85df318533f71d7c507d8ef
Chris Mellor <cmellor@gmail.com>
2021-04-25 20:19:27 +0100

    Add more detail to Dashboard view

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Http\ViewComposers\Fos\PostComposer;
+use App\Models\User;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,25 @@ Logged in as: {{ auth()->user()->name }}
 <p>
     <x-link to="{{ route('fos.index') }}">Admin</x-link>
 </p>
+
+<section>
+    <div>
+        <img alt="{{ auth()->user()->name }}" src="{{ auth()->user()->avatar_path }}">
+    </div>
+    <div>
+        <h2>About me</h2>
+        <p>{{ auth()->user()->about }}</p>
+    </div>
+    <div>
+        <h2>Socials</h2>
+        <div>
+            <div>{{ auth()->user()->social_github }}</div>
+            <div>{{ auth()->user()->social_instagram }}</div>
+            <div>{{ auth()->user()->social_github }}</div>
+        </div>
+    </div>
+</section>
+
 <x-link to="/two-factor-auth">2FA</x-link>
 <x-form.form action="/logout" method="post">
     <input type="submit" value="Log out">
