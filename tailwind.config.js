791ad28aa9ad9a428b72926a2488161fd02ea9b0
Chris Mellor <cmellor@gmail.com>
2021-06-09 17:41:45 +0100

    Adding more Tailwind utilities/components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 module.exports = {
     mode: 'jit',
-    purge: ['./resources/**/*.{js,vue,blade.php}'],
+    purge: [
+        './resources/**/*.{js,vue,blade.php}',
+        './app/Providers/AnimationsServiceProvider.php',
+    ],
     darkMode: 'media', // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                dark: 'hsl(215,15%,16%)',
+                'dark-gray': 'hsl(210,32%,85%)',
+                pizza: 'hsl(29,100%,52%)',
+                'pizza-dark': 'hsl(331,94%,52%)',
+            },
+            fontFamily: {
+                anton: ['Anton', 'sans-serif'],
+                merriweather: ['Merriweather', 'serif'],
+                'roboto-mono': ['Roboto Mono', 'sans-serif'],
+            },
+            transitionTimingFunction: {
+                'out-bounce': 'cubic-bezier(0.68, -0.55, 0.27, 1.55)',
+            },
+            zIndex: {
+                '-10': '-10',
+            },
+        },
     },
     variants: {
         extend: {},
