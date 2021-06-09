a671299bede2d8d60ca4298ec167254950afbecd
Chris Mellor <cmellor@gmail.com>
2021-06-09 17:42:31 +0100

    Added a Service Provider for adding Animations
    ...

---
+++ app/Providers/AnimationsServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class AnimationsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot()
+    {
+        Blade::directive('slideUp', function ($expression) {
+//            [$durationIn, $durationOut] = explode(', ', $expression);
+
+            return <<< ANIMATIONS
+                x-transition:enter="transition ease-out-bounce duration-700"
+                x-transition:enter-start="transform translate-y-full"
+                x-transition:enter-end="transform translate-y-0"
+                x-transition:leave="transition duration-300"
+                x-transition:leave-start="transform translate-y-0"
+                x-transition:leave-end="transform translate-y-full"
+            ANIMATIONS;
+        });
+    }
+}
