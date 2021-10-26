3fed0034a1db3503b85d7034b84ea84947c21655
Chris Mellor <cmellor@gmail.com>
2021-10-27 00:13:03 +0100

    Add dark variant in TW typography plugin
    Update tom-select stylesheet with tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
             transparent: 'transparent',
             current: 'currentColor',
             white: colors.white,
+            black: colors.black,
             'blue-gray': colors.blueGray,
             'cool-gray': colors.coolGray,
             gray: colors.gray,
@@ -60,6 +61,43 @@ module.exports = {
                     },
                 },
             },
+            typography: (theme) => ({
+                dark: {
+                    css: {
+                        color: theme('colors.gray.300'),
+                        '[class~="lead"]': {
+                            color: theme('colors.gray.400'),
+                        },
+                        a: { color: theme('colors.gray.100') },
+                        strong: { color: theme('colors.gray.100') },
+                        'ul > li::before': {
+                            backgroundColor: theme('colors.gray.300'),
+                        },
+                        hr: { borderColor: theme('colors.gray.800') },
+                        blockquote: {
+                            color: theme('colors.gray.100'),
+                            borderLeftColor: theme('colors.gray.400'),
+                        },
+                        h1: { color: theme('colors.gray.100') },
+                        h2: { color: theme('colors.gray.100') },
+                        h3: { color: theme('colors.gray.100') },
+                        h4: { color: theme('colors.gray.100') },
+                        code: { color: theme('colors.gray.100') },
+                        'a code': { color: theme('colors.gray.100') },
+                        pre: {
+                            color: theme('colors.gray.200'),
+                            backgroundColor: theme('colors.gray.800'),
+                        },
+                        thead: {
+                            color: theme('colors.gray.100'),
+                            borderBottomColor: theme('colors.gray.700'),
+                        },
+                        'tbody tr': {
+                            borderBottomColor: theme('colors.gray.800'),
+                        },
+                    },
+                },
+            }),
             zIndex: {
                 '-10': '-10',
             },
