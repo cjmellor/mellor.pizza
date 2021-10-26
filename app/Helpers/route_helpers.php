226d1055c47638867faa7bdf45a9ef9887e83094
Chris Mellor <cjmellor@users.noreply.github.com>
2021-10-26 23:13:35 +0000

    Apply fixes from StyleCI

--- app/Helpers/route_helpers.php
+++ app/Helpers/route_helpers.php
@@ -3,7 +3,7 @@
 /**
  * Determine if you're within a view in the admin section.
  */
-if (!function_exists('isAdminSection')) {
+if (! function_exists('isAdminSection')) {
     function isAdminSection(): bool
     {
         return request()->routeIs('fos.*');
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Model::preventLazyLoading(!$this->app->isProduction());
+        Model::preventLazyLoading(! $this->app->isProduction());
 
         // "Reading Time"
         Str::macro('readingTime', function (...$text) {
