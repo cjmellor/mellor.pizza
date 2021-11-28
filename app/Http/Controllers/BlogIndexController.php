4f7b55bb405d04917b544007b1b46e7d68813ec9
Chris Mellor <cmellor@gmail.com>
2021-11-28 15:26:41 +0000

    Fix cached paginated queries

--- app/Http/Controllers/BlogIndexController.php
+++ app/Http/Controllers/BlogIndexController.php
@@ -13,12 +13,12 @@ class BlogIndexController extends Controller
     public function __invoke(): View
     {
         $posts = Cache::remember(
-            key: 'blog.posts.paginated',
+            key: 'blog.posts.paginated.'.request()->query(key: 'page', default: '1'),
             ttl: now()->addDay(),
             callback: fn () => Post::query()
-                ->with('tags')
+                ->with(relations: 'tags')
                 ->published()
-                ->orderByDesc('id')
+                ->orderByDesc(column: 'id')
                 ->simplePaginate(perPage: self::PER_PAGE),
         );
 
