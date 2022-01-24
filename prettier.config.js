650e796ff5437d7e80f506a7f87f4aa62a488bc5
Chris Mellor <cmellor@gmail.com>
2022-01-24 21:48:15 +0000

    Install Prettier + plugins
    - plugins installed: php; tailwindcss
    - removed ESLint
    - upgrade yarn deps

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 80,
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'es5',
+    phpVersion: '8.0',
+    trailingCommaPHP: true,
+    braceStyle: 'psr-2',
+    requirePragma: false,
+    insertPragma: false,
+    tailwindConfig: './tailwind.config.js',
+};
