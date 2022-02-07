603c17d7cde436662cb14c9d846c103179c523c7
Chris Mellor <cmellor@gmail.com>
2022-02-07 18:48:08 +0000

    Small refactor
    Small refactor

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,15 @@ use App\Http\Controllers\BlogIndexController;
 use App\Http\Controllers\Posts\ShowPostController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'index')
-    ->name('homepage');
+Route::view('/', 'index')->name('homepage');
 
-Route::view('/two-factor-auth', 'auth.two-factor-auth.index')
-    ->name('two-factor-auth');
+Route::view('/about', 'about')->name('about');
 
-Route::get('/blog', BlogIndexController::class)
-    ->name('blog');
+Route::view('/two-factor-auth', 'auth.two-factor-auth.index')->name('two-factor-auth');
 
-Route::get('{slug}', ShowPostController::class)
-    ->name('post.show');
+Route::get('/blog', BlogIndexController::class)->name('blog');
+
+Route::get('{slug}', ShowPostController::class)->name('post.show');
 
 /*
  | For adding and removing attachments to an Editor
