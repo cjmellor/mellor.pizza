b4d616d0f813dcceead590fb63f83c27441520a0
Chris Mellor <cmellor@gmail.com>
2022-06-09 22:00:01 +0100

    fix: Upgrades

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
-const colors = require('tailwindcss/colors');
+/** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    content: ['./resources/**/*.{js,vue,blade.php}', './app/Providers/AnimationsServiceProvider.php', './app/View/**/*.php'],
+    content: [
+        './resources/**/*.{js,vue,blade.php}',
+        './app/Providers/AnimationsServiceProvider.php',
+        './app/View/**/*.php'
+    ],
     theme: {
         extend: {
             animation: {
-                'slide-in-down': 'slideInDown 0.5s',
+                'slide-in-down': 'slideInDown 0.5s'
             },
             colors: {
                 dark: 'hsl(215,15%,16%)',
@@ -15,24 +19,29 @@ module.exports = {
                 'dark-line': 'hsl(214,13%,25%)',
                 'dark-line-lighter': 'hsl(208,13%,30%)',
                 pizza: 'hsl(29,100%,52%)',
-                'pizza-dark': 'hsl(331,94%,52%)',
+                'pizza-dark': 'hsl(331,94%,52%)'
             },
             fontFamily: {
                 anton: ['Anton', 'sans-serif'],
                 merriweather: ['Merriweather', 'serif'],
-                'roboto-mono': ['Roboto Mono', 'sans-serif'],
+                'roboto-mono': ['Roboto Mono', 'sans-serif']
             },
             keyframes: {
                 slideInDown: {
                     from: {
-                        transform: 'translate3d(0, -100%, 0)',
+                        transform: 'translate3d(0, -100%, 0)'
                     },
                     to: {
-                        transform: 'translate3d(0, 0, 0)',
-                    },
-                },
-            },
-        },
+                        transform: 'translate3d(0, 0, 0)'
+                    }
+                }
+            }
+        }
     },
-    plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms'), require('@tailwindcss/line-clamp'), require('@tailwindcss/typography')],
+    plugins: [
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/typography')
+    ]
 };
